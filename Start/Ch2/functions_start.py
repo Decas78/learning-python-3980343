3ab084032c02ff9a2a9c9767cfb35983f632c7d5
Decas78 <97861732+Decas78@users.noreply.github.com>
2025-04-13 18:26:03 +0000

    loops and defining functions (number counter completed externally)

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -3,17 +3,47 @@
 
 
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+
+
+def what_name():
+  print("hello world!")
+  name = input("What is your name? ")
+  print("Nice to meet you,", name)
 
 # function that takes parameters
+def what_name_param(greeting):
+  print("hello world!")
+  name = input("What is your name? ")
+  print(greeting, name)
 
+# what_name_param("G'day")
+# what_name_param("How are you doing today")
 
 # function that returns a value
 
+def add(num1, num2):
+  return num1 + num2
+
+print(add(1, 5))
+
+def cube(x):
+  return x*x*x
+print(cube(10))
 
 # function with default value for an parameter
 
+def hello_func(greeting, name=None):
+  if name == None:
+    name = input("What is your name? ")
+  print(greeting, name)
 
+hello_func("Nice to meet ya")
 # function with variable number of parameters
+
+def multi_add(*args):
+  result = 0
+  for x in args:
+    result = result + x
+  return result
+
+print(multi_add(5, 9, 10, 555))
--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -6,14 +6,32 @@ x = 0
 
 # define a while loop
 
+# while x < 5:
+#   print(x)
+#   x += 1
+
+# answer = ""
+# while answer != "yes":
+#   answer = input("Should I stop?")
 
 # define a for loop
 
+days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+
+# for d in days:
+#   print(d)
 
 # use a for loop over a collection
 
 
 # use the break and continue statements
 
+for d in days:
+  if (d=="Thu"):
+    continue # Skips this iteration so no Thu will be printed
+  print(d)
 
 # using the enumerate() function to get an index and an item
+
+for i,d in enumerate(days):
+  print(i,d)
