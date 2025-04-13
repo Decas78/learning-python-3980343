3bd78ea33ceb90a973759add1f3030fe29be7117
Decas78 <97861732+Decas78@users.noreply.github.com>
2025-04-13 15:22:08 +0000

    variables and simple data types

--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -10,12 +10,22 @@ mystr = "This is a string"
 mybool = True
 
 # We can display the content of a variable using the print() function
-
+print(myint, "\n", myfloat, mystr, mybool)
 
 # Operators are used to perform operations on variables
+print(myint % 4)
 
+another_str = " ANOTHER ONE"
+print(mystr + another_str)
 
 # Logical and comparison operators 
+print(myint == myfloat)
 
+print(myint < 20 or myfloat > 100)
+print(not(myint < 20 or myfloat > 100))
 
 # re-declaring a variable works
+
+myint=33
+
+print(myint)
