2142c550eec62850467f955d0a6ac8efa67fa64e
Decas78 <97861732+Decas78@users.noreply.github.com>
2025-05-13 04:21:28 +0000

    Exceptions and modules

--- Start/Ch3/exceptions_start.py
+++ Start/Ch3/exceptions_start.py
@@ -5,9 +5,23 @@
 # Errors can happen in programs, and we need a clean way to handle them
 # This code will cause an error because you can't divide by zero:
 
+# x = 10 / 0
 # Exceptions provide a way of catching errors and then handling them in 
 # a separate section of the code to group them together
 
+# try:
+#   x = 10 / 0
+# except:
+#   print("Didnt work mate")
 
 # You can also catch specific exceptions
-
+try:
+  answer = input("What Should I Divide 10 by?")
+  num = int(answer)
+  print(10/num)
+except ValueError as e:
+  print("Incorrect Value for Division")
+except ZeroDivisionError as e:
+  print("Sorry but you cant divide by zero")
+finally:
+  print("Finally always runs")
--- Start/Ch3/modules_start.py
+++ Start/Ch3/modules_start.py
@@ -2,27 +2,31 @@
 # Working with modules of code
 
 # import the math module, which contains features for working with mathematics
+import math
 
+print("the square root of 16 =", math.sqrt(16))
 
 # import a specific part of the module so you can refer to it more easily
-
+from math import pi
+print(pi)
 
 # import a module and give it a different name
+import numpy as np
 
 
 # the math module contains lots of pre-built functions
+import random as rand
+
 
 
 # in addition to functions, some modules contain useful constants 
 
 
 # Generate a random number between 100 and 200
-
-
-# try some of the math functions for yourself here:
+r = rand.randint(100,1000)
 
 # Use the 3rd party tabulate module to print tabulated data:
-
+from tabulate import tabulate
 # Sample data
 data = [
   ["Product", "Price", "Stock"],
@@ -32,3 +36,5 @@ data = [
 ]
 
 # Create a formatted table
+
+print(tabulate(data, headers="firstrow", tablefmt="heavy_grid"))
